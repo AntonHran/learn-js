3a5dfabe3f7c40d129fbaa830238e55e66014728
AntonHran <for.program.learning@gmail.com>
2024-03-19 23:10:13 +0200

    first steps

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 "use strict";
+
 const age = 10;
 const totalPrice = 200.75;
 const userName = "Chelsy";
-const message = "Welcome back!";
+// const message = "Welcome back!";
 const isOpen = true;
 
 const type = typeof userName;
 console.log(type);
 
-const shouldExtend = confirm("Do you want to extend your subscription?");
+const message = "Do you want to extend your subscription?";
+const shouldExtend = confirm(message);
 console.log(shouldExtend);
+
+let quantity = prompt("Enter your number");
+try {
+  quantityChanged = Number(quantity);
+  console.log(typeof quantityChanged);
+} catch (TypeError) {
+  console.log("wrong type");
+}
