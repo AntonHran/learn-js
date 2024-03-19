06b9df65ca5e9bd79f28d5b9966c567a4d6d876a
AntonHran <for.program.learning@gmail.com>
2024-03-19 23:35:25 +0200

    work with strings

--- script1.js
+++ script1.js
@@ -1,3 +1,10 @@
 let elementWidth = "50px";
-const result = Number.parseInt(elementWidth);
-console.log(result);
+const result = Number.parseInt(elementWidth); // get integer from string!
+
+console.log("Result: " + result);
+console.log(`Result:`, result);
+console.log(`Result: ${result}`);
+
+let elementHeight = "200.74px";
+elHeight = Number.parseFloat(elementHeight);
+console.log(`elementHeight: ${elHeight}`);
