78fe93ba8ac63d6fe5d681e53b7ab665a5b949e8
AntonHran <for.program.learning@gmail.com>
2024-03-19 22:53:29 +0200

    first steps

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 "use strict";
+const age = 10;
+const totalPrice = 200.75;
+const userName = "Chelsy";
+const message = "Welcome back!";
+const isOpen = true;
+
+const type = typeof userName;
+console.log(type);
+
+const shouldExtend = confirm("Do you want to extend your subscription?");
+console.log(shouldExtend);
