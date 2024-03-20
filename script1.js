6bb318ecf1b9c74558b6b302773b6d2de192c6c3
AntonHran <for.program.learning@gmail.com>
2024-03-20 23:20:09 +0200

    module Math

--- script1.js
+++ script1.js
@@ -13,3 +13,17 @@ let salary = 1300.16472;
 salary = Number(salary.toFixed(2));
 console.log(salary);
 console.log(typeof salary);
+
+// MATH
+const base = 2;
+const power = 5;
+
+const resultPower = Math.pow(base, power);
+console.log(resultPower);
+console.log(2 ** 6);
+
+// Example:
+let baseNumber = prompt("Enter a base: ");
+let powerNumber = prompt("Enter a power for your base: ");
+const res = Number(baseNumber) ** Number(powerNumber);
+alert(`Result is ${res}`);
