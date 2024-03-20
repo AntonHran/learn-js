b9ac393eb25e58aa5ffb203bcc2e646143433920
AntonHran <for.program.learning@gmail.com>
2024-03-20 23:52:11 +0200

    strings

---
+++ script2.js
@@ -0,0 +1,9 @@
+const someString = "skbnc  k ckssn szkhkdjsbn.";
+console.log(someString.length);
+
+// string concatination
+const firstName = "Steven";
+const lastName = "Smith";
+
+const fullName = firstName + " " + lastName;
+console.log(fullName);
