9b4950fd02b1b74b2b45d70ccb6091d7c8ead416
AntonHran <for.program.learning@gmail.com>
2024-03-21 23:25:16 +0200

    work with strings

--- script2.js
+++ script2.js
@@ -11,3 +11,15 @@ console.log(fullName);
 let brand = "SamSung";
 brand = brand.toLowerCase();
 console.log(brand);
+
+const blacklistedWord1 = "spam";
+const blacklistedWord2 = "sale";
+
+const string1 = "Hi there! I am prince Ali, wait, hold on this is not a spam!";
+const string2 = "It is the biggest SALE of the week!";
+
+console.log(string1.includes(blacklistedWord1));
+console.log(string2.includes(blacklistedWord1));
+
+console.log(string1.includes(blacklistedWord2));
+console.log(string2.includes(blacklistedWord2));
