107d08fccbaa5335616f300174f548a8c5eba256
AntonHran <for.program.learning@gmail.com>
2024-03-23 22:04:34 +0200

    edited

--- script2.js
+++ script2.js
@@ -18,11 +18,11 @@ const blacklistedWord2 = "sale";
 const string1 = "Hi there! I am prince Ali, wait, hold on this is not a spam!";
 const string2 = "It is the biggest SALE of the week!";
 
-console.log(string1.includes(blacklistedWord1));
-console.log(string2.includes(blacklistedWord1));
+console.log(string1.toLowerCase().includes(blacklistedWord1));
+console.log(string2.toLowerCase().includes(blacklistedWord1));
 
-console.log(string1.includes(blacklistedWord2));
-console.log(string2.includes(blacklistedWord2));
+console.log(string1.toLowerCase().includes(blacklistedWord2));
+console.log(string2.toLowerCase().includes(blacklistedWord2));
 
 // functions:
 
