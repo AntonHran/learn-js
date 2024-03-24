efdcb4774f99d03dcedb44c4174b0874237b578a
AntonHran <for.program.learning@gmail.com>
2024-03-24 17:55:15 +0200

    concat

--- script3.js
+++ script3.js
@@ -128,3 +128,16 @@ console.log(languages); // ["C", "Python", "Java", "JavaScript"]
 // Заміняємо один елемент (з індексом 2) на декілька
 languages.splice(2, 1, "C#", "Swift", "Go");
 console.log(languages); // ["C", "Python", "C#", "Swift", "Go", "JavaScript"]
+
+// concat()
+const oldClients = ["Mango", "Ajax", "Poly", "Kiwi"];
+const newClients = ["Monkong", "Singu"];
+
+const allClientsWithOldFirst = oldClients.concat(newClients);
+console.log(allClientsWithOldFirst); // ["Mango", "Ajax", "Poly", "Kiwi", "Monkong", "Singu"]
+
+const allClientsWithNewFirst = newClients.concat(oldClients);
+console.log(allClientsWithNewFirst); // ["Monkong", "Singu", "Mango", "Ajax", "Poly", "Kiwi"]
+
+console.log(oldClients); // ["Mango", "Ajax", "Poly", "Kiwi"]
+console.log(newClients); // ["Monkong", "Singu"]
