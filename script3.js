04435ed51cced70ea0673119b5e61c6086ba505f
AntonHran <for.program.learning@gmail.com>
2024-03-24 17:54:00 +0200

    slice-splice

--- script3.js
+++ script3.js
@@ -99,3 +99,32 @@ console.log(numbers); // [1]
 
 console.log(numbers.pop()); //  1
 console.log(numbers); // []
+
+// slice()
+const clients2 = ["Mango", "Ajax", "Poly", "Kiwi"];
+console.log(clients2.slice(1, 3)); // ["Ajax", "Poly"]
+console.log(clients2.slice(2)); // ["Poly", "Kiwi"]
+console.log(clients2.slice(-2)); // ["Poly", "Kiwi"]
+
+// splice()
+const scores = [1, 2, 3, 4, 5];
+// Видаляємо три елементи масиву, починаючи з першого елемента (індекс 0)
+const deletedScores = scores.splice(0, 3);
+// Тепер масив scores містить два елементи
+console.log(scores); // [4, 5]
+// А масив deletedScores містить три видалені елементи
+console.log(deletedScores); // [1, 2, 3]
+
+const colors = ["red", "green", "blue"];
+colors.splice(2, 0, "purple");
+console.log(colors); // ["red", "green", "purple", "blue"]
+colors.splice(1, 0, "yellow", "pink");
+console.log(colors);
+
+const languages = ["C", "C++", "Java", "JavaScript"];
+// Заміняємо елемент з індексом 1 на новий
+languages.splice(1, 1, "Python");
+console.log(languages); // ["C", "Python", "Java", "JavaScript"]
+// Заміняємо один елемент (з індексом 2) на декілька
+languages.splice(2, 1, "C#", "Swift", "Go");
+console.log(languages); // ["C", "Python", "C#", "Swift", "Go", "JavaScript"]
