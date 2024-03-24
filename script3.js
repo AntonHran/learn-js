a737b1bf2e3263d806887d3d2d17be2a788131e8
AntonHran <for.program.learning@gmail.com>
2024-03-24 17:30:45 +0200

    split-join

--- script3.js
+++ script3.js
@@ -42,3 +42,17 @@ const string = "javascript";
 for (const character of string) {
   console.log(character);
 }
+
+// massives - lists in python
+const name = "Mango";
+console.log(name.split("")); // ["M", "a", "n", "g", "o"]
+
+const message = "JavaScript - це цікаво";
+console.log(message.split(" ")); // ["JavaScript", "-", "це", "цікаво"]
+
+// join
+
+const words = ["JavaScript", "це", "цікаво"];
+console.log(words.join("")); // "JavaScriptцецікаво"
+console.log(words.join(" ")); // "JavaScript це цікаво"
+console.log(words.join("-")); // "JavaScript-це-цікаво"
