b6a1b06a6569c83e707859cc0b672a69385b0d18
AntonHran <for.program.learning@gmail.com>
2024-03-25 23:15:21 +0200

    computed properties

--- script5.js
+++ script5.js
@@ -16,7 +16,7 @@ console.log(book);
 
 // new example
 const user = {
-  name: "Jacques Gluke",
+  name_: "Jacques Gluke",
   tag: "jgluke",
   location: {
     country: "Jamaica",
@@ -32,7 +32,7 @@ const country = user.location.country;
 console.log(country); // 'Jamaica'
 console.log(location1.city);
 
-const userName = user["name"];
+const userName = user["name_"];
 console.log(userName);
 
 // shorthand properties
@@ -51,3 +51,13 @@ const userNew = {
   name,
   age,
 };
+
+// computed properties
+const propName = "name";
+const user3 = {
+  age: 25,
+  // Ім'я цієї властивості буде взяте зі значення змінної propName
+  [propName]: "Генрі Сибола",
+};
+
+console.log(user3.name); // 'Генрі Сибола'
