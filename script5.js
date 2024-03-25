02a57cd22872ba2ab483fd18ba67514db237a9ca
AntonHran <for.program.learning@gmail.com>
2024-03-25 22:59:10 +0200

    new script

---
+++ script5.js
@@ -0,0 +1,25 @@
+// objects - dictionaries
+const book = {
+  title: "The Last Kingdom",
+  author: "Bernard Cornwell",
+  genres: ["historical prose", "adventure"],
+  isPublic: true,
+  rating: 8.38,
+};
+console.log(book);
+
+const user = {
+  name: "Jacques Gluke",
+  tag: "jgluke",
+  location: {
+    country: "Jamaica",
+    city: "Ocho Rios",
+  },
+  hobbies: ["swiming", "music", "sci-fi"],
+};
+
+const location1 = user.location;
+console.log(location1); // Об'єкт location
+
+const country = user.location.country;
+console.log(country); // 'Jamaica'
