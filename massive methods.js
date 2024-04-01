f00d6f9b36c07c380d3c2666a54c1201dbb6635d
AntonHran <for.program.learning@gmail.com>
2024-04-01 22:38:14 +0300

    forEach() ex.

--- massive methods.js
+++ massive methods.js
@@ -79,3 +79,23 @@ repeat(3, prettyPrint);
 // Logging value: 0
 // Logging value: 1
 // Logging value: 2
+
+////////////////////////////////////////////////////////////////////////////
+
+// forEach()
+// массив.forEach(function callback(element, index, array) {
+// Тіло колбек-функції
+// });
+// return nothing
+
+const numbers = [5, 10, 15, 20, 25];
+
+// Класичний for
+for (let i = 0; i < numbers.length; i += 1) {
+  console.log(`Індекс ${i}, значення ${numbers[i]}`);
+}
+
+// Метод перебирання forEach
+numbers.forEach(function (number, index) {
+  console.log(`Індекс ${index}, значення ${number}`);
+});
