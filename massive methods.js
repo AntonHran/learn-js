ddf0b6a0ca64492790967727acc11a41f934298a
AntonHran <for.program.learning@gmail.com>
2024-04-01 23:42:19 +0300

    new ex.

--- massive methods.js
+++ massive methods.js
@@ -160,3 +160,31 @@ const students = [
 
 const names = students.map((student) => student.name);
 console.log(names); // ['Манго', 'Полі', 'Аякс', 'Ківі', 'Х'юстон']
+
+// tests
+const pizzaPalace = {
+  pizzas: ["Ultracheese", "Smoked", "Four meats"],
+  order(pizzaName, onSuccess, onError) {
+    if (this.pizzas.includes(pizzaName)) {
+      return onSuccess(pizzaName);
+    } else {
+      return onError(
+        `There is no pizza with a name ${pizzaName} in the assortment.`
+      );
+    }
+  },
+};
+// Change code above this line
+
+// Callback for onSuccess
+function makePizza(pizzaName) {
+  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+}
+
+// Callback for onError
+function onOrderError(error) {
+  return `Error! ${error}`;
+}
+
+// Method calls with callbacks
+console.log(pizzaPalace.order("Smoked", makePizza, onOrderError));
