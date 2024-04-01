b4317ff64e41d3896b3752f41d2c82a782277765
AntonHran <for.program.learning@gmail.com>
2024-04-01 23:04:00 +0300

    map ex.

--- massive methods.js
+++ massive methods.js
@@ -132,3 +132,31 @@ add1(1, 2, 3); // [1, 2, 3]
 numbers.forEach((number, index) => {
   console.log(`Індекс ${index}, значення ${number}`);
 });
+
+// map
+// массив.map((element, index, array) => {
+// Тіло колбек-функції
+// });
+// return new massive
+
+const planets = ["Земля", "Марс", "Венера", "Юпітер"];
+
+const planetsInUpperCase = planets.map((planet) => planet.toUpperCase());
+console.log(planetsInUpperCase); // ['ЗЕМЛЯ', 'МАРС', 'ВЕНЕРА', 'ЮПІТЕР']
+
+const planetsInLowerCase = planets.map((planet) => planet.toLowerCase());
+console.log(planetsInLowerCase); // ['земля', 'марс', 'венера', 'юпітер']
+
+// Оригінальний масив не змінився
+console.log(planets); // ['Земля', 'Марс', 'Венера', 'Юпітер']
+
+const students = [
+  { name: "Манго", score: 83 },
+  { name: "Полі", score: 59 },
+  { name: "Аякс", score: 37 },
+  { name: "Ківі", score: 94 },
+  { name: "Х'юстон", score: 64 },
+];
+
+const names = students.map((student) => student.name);
+console.log(names); // ['Манго', 'Полі', 'Аякс', 'Ківі', 'Х'юстон']
