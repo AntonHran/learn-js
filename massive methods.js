061c5c3b79c561fdc62c18753e8c388dd2bb759c
AntonHran <for.program.learning@gmail.com>
2024-04-02 22:46:29 +0300

    filter ex.

--- massive methods.js
+++ massive methods.js
@@ -198,3 +198,35 @@ function onOrderError(error) {
 
 // Method calls with callbacks
 console.log(pizzaPalace.order("Smoked", makePizza, onOrderError));
+
+// filter
+// масив.filter((element, index, array) => {
+// Тіло колбек-функції
+// });
+const values = [51, -3, 27, 21, -68, 42, -37];
+
+const positiveValues = values.filter((value) => value >= 0);
+console.log(positiveValues); // [51, 27, 21, 42]
+
+const negativeValues = values.filter((value) => value < 0);
+console.log(negativeValues); // [-3, -68, -37]
+
+const bigValues = values.filter((value) => value > 1000);
+console.log(bigValues); // []
+
+// Оригінальний масив не змінився
+console.log(values); // [51, -3, 27, 21, -68, 42, -37]
+
+const studentsCourses = [
+  { name: "Манго", courses: ["математика", "фізика"] },
+  { name: "Полі", courses: ["інформатика", "математика"] },
+  { name: "Ківі", courses: ["фізика", "біологія"] },
+];
+
+const allCourses = studentsCourses.flatMap((student) => student.courses);
+// ['математика', 'фізика', 'інформатика', 'математика', 'фізика', 'біологія'];
+
+const uniqueCourses = allCourses.filter(
+  (course, index, array) => array.indexOf(course) === index
+);
+console.log(uniqueCourses);
