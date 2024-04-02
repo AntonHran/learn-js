b806bdd33a47ff9f7fa4c3e5371977606e45bf2d
AntonHran <for.program.learning@gmail.com>
2024-04-02 22:54:06 +0300

    find ex.

--- massive methods.js
+++ massive methods.js
@@ -230,3 +230,19 @@ const uniqueCourses = allCourses.filter(
   (course, index, array) => array.indexOf(course) === index
 );
 console.log(uniqueCourses);
+
+// find
+// масив.find((element, index, array) => {
+// Тіло колбек-функції
+// });
+const colorPickerOptions = [
+  { label: "red", color: "#F44336" },
+  { label: "green", color: "#4CAF50" },
+  { label: "blue", color: "#2196F3" },
+  { label: "pink", color: "#E91E63" },
+  { label: "indigo", color: "#3F51B5" },
+];
+
+colorPickerOptions.find((option) => option.label === "blue"); // { label: 'blue', color: '#2196F3' }
+colorPickerOptions.find((option) => option.label === "pink"); // { label: 'pink', color: '#E91E63' }
+colorPickerOptions.find((option) => option.label === "white"); // undefined
