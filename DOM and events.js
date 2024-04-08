5ce06b8603266c654a51836ae97c6f3e4b99a148
AntonHran <for.program.learning@gmail.com>
2024-04-08 23:09:10 +0300

    edited

--- DOM and events.js
+++ DOM and events.js
@@ -9,3 +9,12 @@ console.log(menuItemsByTagName);
 
 const menuItemsByClass = document.querySelectorAll(".menu-item");
 console.log(menuItemsByClass);
+
+const notExistSelector = document.querySelector(".itemdvfgb");
+console.log(notExistSelector);
+
+const selectThroughEl = ulEl.querySelector(".menu-item");
+console.log(selectThroughEl);
+
+const selectThroughElAll = ulEl.querySelectorAll(".menu-item");
+console.log(selectThroughElAll);
