fc1b642fbae9f254959dd5e5b236a9edd0f4b96d
AntonHran <for.program.learning@gmail.com>
2024-04-08 22:49:17 +0300

    begin

---
+++ DOM and events.js
@@ -0,0 +1,11 @@
+const listWithClass = document.querySelector(".menu");
+console.log(listWithClass);
+
+const ulEl = document.querySelector("ul");
+console.log(ulEl);
+
+const menuItemsByTagName = document.querySelectorAll("li");
+console.log(menuItemsByTagName);
+
+const menuItemsByClass = document.querySelectorAll(".menu-item");
+console.log(menuItemsByClass);
