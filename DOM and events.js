38bcf9833a23f409696111b62a62cd5f4481bfd2
AntonHran <for.program.learning@gmail.com>
2024-04-09 22:53:50 +0300

    some built in 'interface'

--- DOM and events.js
+++ DOM and events.js
@@ -32,3 +32,10 @@ const heroTitleEl = document.querySelector(".hero_title");
 console.log(heroTitleEl);
 const textHeroTitleEl = heroTitleEl.textContent;
 console.log(textHeroTitleEl);
+
+// interface
+console.log(imageEl.getAttribute("src")); // that is equal to
+console.log(imageEl.src);
+
+console.log(imageEl.hasAttribute("alt"));
+console.log(imageEl.hasAttribute("khkbk"));
