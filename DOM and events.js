201a87a60abee5326e29119fb5cf659d509fc47b
AntonHran <for.program.learning@gmail.com>
2024-04-12 23:33:39 +0300

    elements collection

--- DOM and events.js
+++ DOM and events.js
@@ -97,3 +97,30 @@ navItemEl.appendChild(navLinkEl);
 console.log(navItemEl);
 
 navElNav.appendChild(navItemEl);
+navElNav.insertBefore(navItemEl, navElNav.firstElementChild);
+
+const heroEl = document.querySelector(".hero");
+heroEl.append(titleEl, imageEl);
+
+// elements collection
+const ColorPickerOptions = [
+  { label: "red", color: "#F44336" },
+  { label: "green", color: "#4CAF50" },
+  { label: "blue", color: "#2196F3" },
+  { label: "grey", color: "#607D8B" },
+  { label: "pink", color: "#E91E63" },
+  { label: "indigo", color: "#3F51B5" },
+];
+
+const colorPickerContainerEl = document.querySelector(".js-color-picker");
+
+const elements = ColorPickerOptions.map((element) => {
+  const buttonEl = document.createElement("button");
+  buttonEl.type = "button";
+  buttonEl.classList.add("color-picker__option");
+  buttonEl.textContent = element.label;
+  buttonEl.style.backgroundColor = element.color;
+  return buttonEl;
+});
+
+colorPickerContainerEl.append(...elements);
