aca5d243a3e2df011a95a7f47c0bb2fd83c5d0e7
AntonHran <for.program.learning@gmail.com>
2024-04-13 23:19:49 +0300

    init

---
+++ data/transactions.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    id: "758d5283-358e-4fbb",
+    amount: 179.07,
+    date: "2012-02-01",
+    businnes: "Bogan-DuBuque",
+    name: "Auto Loan Account 7313",
+    type: "deposit",
+    account: "1980943",
+  },
+  {
+    id: "f0463ec4-7104-4adb",
+    amount: 1930.87,
+    date: "2012-02-07",
+    businnes: "Legros, Weimann and Treutel",
+    name: "Auto Loan Account 0721",
+    type: "invoice",
+    account: "38277848",
+  },
+  {
+    id: "6c0e5a78-ad9f-46af",
+    amount: 704.53,
+    date: "2012-02-11",
+    businnes: "Beatty, Wisozk and Koch",
+    name: "Savings Account 1894",
+    type: "withdrawal",
+    account: "76727204",
+  },
+  {
+    id: "16bd2a9d-7b0e-a75c",
+    amount: 656.81,
+    date: "2012-02-02",
+    businnes: "Hane-Bode",
+    name: "Personal Loan Account 2316",
+    type: "withdrawal",
+    account: "274462350",
+  },
+];
