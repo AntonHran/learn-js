50000616db2fa19ec685aa1fa866e827e75f74f8
AntonHran <for.program.learning@gmail.com>
2024-04-13 21:17:28 +0300

    init

---
+++ data/products.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    name: "Servodrive",
+    desription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, sit qui voluptatum iste necessitatibus alias architecto aliquid fugiat quia. Debitis?",
+    price: 2000,
+    available: true,
+    onSale: true,
+  },
+  {
+    name: "Field generator",
+    desription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, sit qui voluptatum iste necessitatibus alias architecto aliquid fugiat quia. Debitis?",
+    price: 3000,
+    available: false,
+    onSale: false,
+  },
+  {
+    name: "Zero element",
+    desription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, sit qui voluptatum iste necessitatibus alias architecto aliquid fugiat quia. Debitis?",
+    price: 1500,
+    available: true,
+    onSale: false,
+  },
+  {
+    name: "Titanium",
+    desription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, sit qui voluptatum iste necessitatibus alias architecto aliquid fugiat quia. Debitis?",
+    price: 2500,
+    available: false,
+    onSale: false,
+  },
+  {
+    name: "Something else",
+    desription:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, sit qui voluptatum iste necessitatibus alias architecto aliquid fugiat quia. Debitis?",
+    price: 1000,
+    available: true,
+    onSale: true,
+  },
+];
