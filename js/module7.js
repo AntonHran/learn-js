b45ba076ac6b4c26cd571f1e77341cb904afb2ba
AntonHran <for.program.learning@gmail.com>
2024-04-22 23:37:07 +0300

    edited

---
+++ js/module7.js
@@ -0,0 +1,18 @@
+const parent = document.querySelector("#parent");
+const child = document.querySelector("#child");
+const descendant = document.querySelector("#descendant");
+
+parent.addEventListener("click", (event) => {
+  console.log(`Event target - ${event.target}`);
+  console.log("Parent click handler");
+});
+
+child.addEventListener("click", (event) => {
+  console.log(`Event target - ${event.target}`);
+  console.log("Child click handler");
+});
+
+descendant.addEventListener("click", (event) => {
+  console.log(`Event target - ${event.target}`);
+  console.log("Descendant click handler");
+});
