95bc80c2cd1576e7e7dac1b4491c667a053b205b
AntonHran <for.program.learning@gmail.com>
2024-05-08 23:34:11 +0300

    example

--- js/module8.js
+++ js/module8.js
@@ -1,2 +1,19 @@
 const message = "NodeJS is amazing!";
 console.log(message);
+
+const sum = (a, b) => a + b;
+sum(2, 3);
+
+class Hero {
+  constructor(name) {
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+}
